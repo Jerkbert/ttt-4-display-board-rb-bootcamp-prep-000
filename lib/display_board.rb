cb132814787c0cea8a11875e3f5d32680166eb25
Rob Ballantyne <raballantyne@gmail.com>
2019-07-12 18:53:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(game=" ")
+def display_board(game="   ")
   puts "#{game[0]}|#{game[1]}|#{game[2]}"
   puts "-----------"
   puts "#{game[3]}|#{game[4]}|#{game[5]}"
